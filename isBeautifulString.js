4b6fe75adf5e6366c579a56f071ba1eddf57fe14
Sandra Coburn <sandracoburn@ezs-macbook-pro.lan>
2021-07-07 18:45:01 -0700

    isBeautifulString javascript

---
+++ isBeautifulString.js
@@ -0,0 +1,17 @@
+function isBeautiful(str) {
+  const alphabet = [];
+  for (let i = 'a'.charCodeAt(); i <= 'z'.charCodeAt(); i++) {
+    alphabet.push(String.fromCharCode(i));
+  }
+  const letters = Array(alphabet.length).fill(0);
+  for (let letter of str) {
+    letters[alphabet.indexOf(letter)]++;
+  }
+  for (let i = 0; i < letters.length; i++) {
+    if (letters[i + 1] > letters[i]) return false;
+  }
+  return true;
+}
+
+console.log(isBeautiful('bbbaacdafe')); //true
+console.log(isBeautiful('macarena')); //false
